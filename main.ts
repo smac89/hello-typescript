d5a84b051af405c5df8fdbb7d1b75ed25949938c
smac89 <smac89@users.noreply.github.com>
2017-06-22 22:41:55 -0600

    Finished with this small example

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ let g = new Map<string, number>([
 
 function es6MapToJSON<K, V>(map: Map<K, V>): {K: V} {
     return Array.from(map.entries()).reduce((objMap, entry) => {
-        objMap[entry[0]] = entry[1];
+        objMap[entry[0]] = (entry[1] instanceof Map ? es6MapToJSON(<any>entry[1]): entry[1]);
         return objMap;
     }, Object.create(null));
 }
@@ -29,3 +29,12 @@ for (const entry of Array.from(g.entries())) {
 }
 
 console.log(es6MapToJSON(models));
+
+let myObj = es6MapToJSON(models);
+
+console.log(Object.assign(myObj, {
+	"Hi": 1,
+	"World": 2
+}));
+
+console.log(models instanceof Map);
